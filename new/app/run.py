1487d97371ca1d15b344878d0b8b2fe8786e6cc7
HuangYan <huangyanxizang@163.com>
2018-10-09 12:05:44 +0800

    Changed the database tablename

--- new/app/run.py
+++ new/app/run.py
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/disaster.db')
-df = pd.read_sql_table('disaster', engine)
+engine = create_engine('sqlite:///DisasterResponse.db')
+df = pd.read_sql_table('DisasterResponse', engine)
 
 # load model
-model = joblib.load("../models/model.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
